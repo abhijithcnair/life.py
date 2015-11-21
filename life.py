f1ac53bd4b2dde3deedb652cd754b75aa3561567
abhijithcnair <abhijithcnair@yahoo.com>
2015-11-21 22:05:52 +0530

    with grid dimension, neighbours are located

--- life.py
+++ life.py
@@ -5,7 +5,20 @@ grid= [[0,0,0],[1,1,1],[0,0,0]]
 def life(grid, x,y):
     return grid[x][y]==1
 
-
+def alive_neighbours(grid, x,y,):
+    count=0
+    for gx in (-1,0,1):
+        for gy in (-1,0,1):
+            if gx==gy==0:
+                continue
+            
+            nx,ny= x+gx,y+gy
+            if nx<0 or ny<0:
+                continue
+            try:
+                count= count+grid[nx][ny]
+            except IndexError:
+                continue
             
             
             
